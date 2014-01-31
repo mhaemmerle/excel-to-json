f96a3c885730f1530169c8640eb542ca7854ce59
Marc Haemmerle <mail@marchaemmerle.com>
2014-01-31 15:13:12 +0100

    bump deps

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject excel-to-json "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.cli "0.2.2"]
-                 [cheshire "4.0.3"]
+                 [cheshire "5.3.1"]
                  [myguidingstar/clansi "1.3.0"]
                  [clojure-watch "0.1.9"]
-                 [org.flatland/ordered "1.5.1"]
+                 [org.flatland/ordered "1.5.2"]
                  [clj-excel "0.0.1"]]
   :plugins [[lein-marginalia "0.7.1"]]
   :jvm-opts ["-Djava.awt.headless=true"]
