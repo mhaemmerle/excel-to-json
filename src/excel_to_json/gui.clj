2fc27bba5b14de37c4706cc0104bcc1e4017e572
Marc Haemmerle <mail@marchaemmerle.com>
2014-02-05 14:26:41 +0100

    use :open to not default to a generic file dialog which wrongly appends selected folder to path

--- src/excel_to_json/gui.clj
+++ src/excel_to_json/gui.clj
@@ -33,7 +33,7 @@
 (defn get-select-button [ch tag]
   (let [text (keyword (str "#" (name tag) "-text"))
         handler (fn [event]
-                  (when-let [file (sch/choose-file :type "Select"
+                  (when-let [file (sch/choose-file :type :open
                                                    :selection-mode :dirs-only)]
                     (sc/text! (sc/select (sc/to-root event) [text]) (.getPath file))
                     (put-preference (str (name tag) "-directory") (.getPath file))
